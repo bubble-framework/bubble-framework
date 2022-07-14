79ff8a3fd543d5d152a97e2dee403cbc71f76753
Missy Lovegren <audrey.lovegren@gmail.com>
2022-07-14 11:18:39 -0500

    Remove unused imports in destroy.js

--- src/commands/destroy.js
+++ src/commands/destroy.js
@@ -1,7 +1,5 @@
-const { deleteUserAll } = require('../util/deleteUser');
 const { deleteApps } = require('../util/deleteApps');
-const { deleteDatabase } = require("../util/deleteDatabase")
-const { deleteLocalFiles } = require('../util/deleteLocalFiles')
+const { deleteLocalFiles } = require('../util/deleteLocalFiles');
 
 const destroy = async () => {
   await deleteApps();
