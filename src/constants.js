9cbba3e12368b3bf2657bd1651f222c9542e877b
Missy Lovegren <audrey.lovegren@gmail.com>
2022-07-15 14:25:13 -0500

    Adjust invocation of asynchronous function in constants again

--- src/constants.js
+++ src/constants.js
@@ -1,14 +1,12 @@
 const { wrapExecCmd } = require("./util/wrapExecCmd");
 
-async function getRepoInfo() {
+const repoInfo = (async function getRepoInfo() {
   let nameWithOwner = await wrapExecCmd(
     "gh repo view --json nameWithOwner -q '.nameWithOwner'"
   );
 
   const [ owner, repo ] = nameWithOwner.split('/');
   return { owner, repo };
-};
-
-const repoInfo = await getRepoInfo();
+})();
 
 module.exports = { repoInfo };
