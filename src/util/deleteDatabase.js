6b83dd87b85d8cf8f165efcb973b986933506418
Missy Lovegren <audrey.lovegren@gmail.com>
2022-07-18 14:34:31 -0500

    Appease eslint for deleteDatabase.js

--- src/util/deleteDatabase.js
+++ src/util/deleteDatabase.js
@@ -1,19 +1,24 @@
-import { wrapExecCmd } from "../util/wrapExecCmd";
+import { wrapExecCmd } from './wrapExecCmd';
 
-import { deleteTable } from '../services/awsService';
+import awsService from '../services/awsService';
 import { getRepoInfo } from '../constants';
 
-import { bubbleGeneral, bubbleSuccess, bubbleErr, bubbleWarn } from "./logger";
+import {
+  bubbleGeneral,
+  bubbleSuccess,
+  bubbleErr,
+  bubbleWarn,
+} from './logger';
 
-import { dbDeletionError } from "./messages";
+import { dbDeletionError } from './messages';
 
 const deleteDatabase = async (name) => {
   const { repo } = await getRepoInfo();
   bubbleGeneral(`Deleting the ${repo}-${name} database...`);
 
   try {
-    await wrapExecCmd(deleteTable(repo, name));
-    bubbleSuccess("deleted", `${name} database: `);
+    await wrapExecCmd(awsService.deleteTable(repo, name));
+    bubbleSuccess('deleted', `${name} database: `);
   } catch (err) {
     bubbleErr(err);
     bubbleWarn(dbDeletionError(repo, name));
