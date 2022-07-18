4a2ea0d58a5489dd5fe60d2ab693ca894e66a98f
Missy Lovegren <audrey.lovegren@gmail.com>
2022-07-18 18:25:32 -0500

    Appease eslint for encrypt.js

--- src/util/encrypt.js
+++ src/util/encrypt.js
@@ -1,16 +1,17 @@
-const sodium = require("libsodium-wrappers");
+// eslint-disable-next-line camelcase
+import { ready, crypto_box_seal } from 'libsodium-wrappers';
 
 const encrypt = async (publicKey, secretVal) => {
-  await sodium.ready;
+  await ready;
   const key = publicKey;
 
   const messageBytes = Buffer.from(secretVal);
-  const keyBytes = Buffer.from(key, "base64");
+  const keyBytes = Buffer.from(key, 'base64');
 
-  const encryptedBytes = sodium.crypto_box_seal(messageBytes, keyBytes);
+  const encryptedBytes = crypto_box_seal(messageBytes, keyBytes);
 
-  const encrypted = Buffer.from(encryptedBytes).toString("base64");
+  const encrypted = Buffer.from(encryptedBytes).toString('base64');
   return encrypted;
 };
 
-module.exports = { encrypt };
+export default { encrypt };
