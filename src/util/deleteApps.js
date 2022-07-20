cc5636c372a13eca671ec0724c07486d01cd155f
Julia Shi <julia.shi@nyu.edu>
2022-07-20 11:31:18 +0100

    replace manual header with header obj from constant

--- src/util/deleteApps.js
+++ src/util/deleteApps.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 import awsService from '../services/awsService.js';
 import wrapExecCmd from './wrapExecCmd.js';
-import { getRepoInfo } from '../constants.js';
+import { getRepoInfo, GH_HEADER_OBJ } from '../constants.js';
 import { readConfigFile } from './fs.js';
 import { configPath } from './paths.js';
 
@@ -38,13 +38,6 @@ const triggerRemoteRepoAppsTeardown = async ({ owner, repo, pullRequestIds }) =>
   const url = `https://api.github.com/repos/${owner}/${repo}/actions/workflows/${DELETE_ALL_WORKFLOW_FILE}/dispatches`;
   const token = getGitHubToken();
 
-  const headerData = {
-    headers: {
-      accept: 'application/vnd.github+json',
-      authorization: `token ${token}`,
-    },
-  };
-
   const body = {
     ref: 'main',
     inputs: {
@@ -53,7 +46,7 @@ const triggerRemoteRepoAppsTeardown = async ({ owner, repo, pullRequestIds }) =>
   };
 
   try {
-    await axios.post(url, body, headerData);
+    await axios.post(url, body, GH_HEADER_OBJ);
   } catch (e) {
     if (e.response.status === 422 && e.response.data.message.includes('pr-numbers')) {
       bubbleWarn('Looks like there are no preview apps to be deleted for this repository!');
