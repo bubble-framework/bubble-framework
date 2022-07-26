ece3ec372a6f60a8ffcd981ae200b8eef20f2724
Missy Lovegren <audrey.lovegren@gmail.com>
2022-07-26 14:10:11 -0500

    Trim whitespace from returned Lambda prefixes using regexp instead of split

--- src/util/deleteLambdas.js
+++ src/util/deleteLambdas.js
@@ -8,13 +8,14 @@ const deleteLambdas = async () => {
   const { repo } = await getRepoInfo();
   let prefixes = JSON.parse(await wrapExecCmd(awsService.getLambdaPrefixFromDb(repo)));
   prefixes = prefixes.Items.flatMap((lambda) => lambda.LambdaPrefix.S);
+  console.log('prefixes', prefixes);
 
   let functionNames = await Promise.all(prefixes.map((prefix) => (
     wrapExecCmd(awsService.getLambdaFunctions(prefix, repo))
   )));
-
+    
   functionNames = functionNames
-    .map((functionName) => functionName.trim())
+    .flatMap((functionName) => functionName.split(/\s/))
     .filter((functionName) => !!functionName);
 
   try {
